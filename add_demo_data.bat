29f999035c18d1f253b6ee760fcbb68e078653ef
benthrntn <bphthornton@icloud.com>
2026-05-29 04:14:02 -0700

    removed filler data from sourcecode

--- add_demo_data.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo  ShareaSpot - Adding Demo Data
-echo ========================================
-echo.
-cd /d "%~dp0backend"
-python seed_data.py
-echo.
-pause
